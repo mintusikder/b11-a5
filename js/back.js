da3a05b024b405f4cdf6363f473f2cefb9f63bdf
Mintu Sikder <mintusikder15@gmail.com>
2025-03-01 20:27:45 +0600

    added back home page

---
+++ js/back.js
@@ -0,0 +1,9 @@
+// new-today
+document.getElementById("new-today").addEventListener("click", function () {
+  window.location.href = "../question.html";
+});
+
+//back-desk
+document.getElementById("back-desk").addEventListener("click", function () {
+  window.location.href = "../index.html";
+});
--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-// new-today
-// document.getElementById("new-today").addEventListener("click", function () {
-//   window.location.href = "../question.html";
-// });
 
-//back-desk
-// document.getElementById("back-desk").addEventListener("click", function () {
-//   window.location.href = "../index.html";
-// });
 
 //   bg color change
 function changeColor() {
