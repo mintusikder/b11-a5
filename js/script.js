8faf14491ab4d1ea96a13d2802f5a2a42dd92e41
Mintu Sikder <mintusikder15@gmail.com>
2025-03-01 19:16:35 +0600

    added random color

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@ document.getElementById("new-today").addEventListener("click", function () {
 });
 //back-desk
 document.getElementById("back-desk").addEventListener("click", function () {
-    window.location.href = "../index.html"; 
-  });
+  window.location.href = "../index.html";
+});
+
+//   bg color change
+function changeColor() {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.body.style.backgroundColor = randomColor;
+}
